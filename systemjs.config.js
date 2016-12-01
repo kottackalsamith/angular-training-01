7c102d2d270e69df5cf7524db0125e300e2cf9a6
samith <kottackalsamith@gmail.com>
2016-12-01 19:07:09 +0530

    Initalized project

---
+++ systemjs.config.js
@@ -0,0 +1,33 @@
+System.config({
+    baseURL: "/",
+    defaultJSExtensions: true,
+    paths: {
+        // paths serve as alias
+        'bower:*': 'bower_components/*'
+    },
+    map: {
+        // our app is within the app folder
+        app: 'app',
+        // Angular Modules
+        "angular": "bower:angular/angular.js",
+        "angular-route": "bower:angular-route/angular-route.js"
+    },
+    packages: {
+        app: {
+            main: './app.js',
+            defaultExtension: 'js'
+        }
+    },
+    meta: {
+        'angular': {
+            format: 'global',
+            exports: 'angular'
+        },
+        'angular-route': {
+            "deps": [
+                "angular"
+            ]
+        }
+    }
+
+});
